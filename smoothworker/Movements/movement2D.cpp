49226185cf9fa83f192a4c2da2036f1b9a939847
vn_sunday <vnsunday@vnsunday.vn>
2026-05-10 22:00:02 +0700

    2d

---
+++ smoothworker/Movements/movement2D.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    /*================================================== 
+        Configuration
+     *==================================================*/
+    double dR_M2P = 100.0; // Meter 2 Pixel
+    double dXc = 10; // In Meter 
+    double dYc =10;
+
+    double dMarginX = 0.2; // Meter 
+    double dMarginY = 0.2; // Meter 
+
+    double dRadius; // In Meters 
+    double dVector; // 
+
+    // Meter 2 Pixel 
+    return 0;
+}
