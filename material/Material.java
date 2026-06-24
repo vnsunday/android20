04610c0dbe8cc2586098b8c97d147b085da80254
vn_sunday <vnsunday@vnsunday.vn>
2026-06-24 19:48:03 +0700

    Material

---
+++ material/Material.java
@@ -0,0 +1,17 @@
+package com.android20;
+
+class Material {
+    public static void main(String[] args) {
+       
+        /*============================================================           
+            Constant
+         *============================================================*/
+        String [] azTypes = {
+            "Polyeste", "Nylon", "Cotton", "Bamboo", "Silk", "Denim" 
+        };
+        
+        String [][] azAttributes = {
+            { "Polyeste", "type: polyme" }
+        };
+    }
+}
