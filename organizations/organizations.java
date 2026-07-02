e0700b7ea29d7b8e1b7b8068324406a6811a122a
vn_sunday <vnsunday@vnsunday.vn>
2026-07-02 10:56:18 +0700

    organizations

---
+++ organizations/organizations.java
@@ -0,0 +1,13 @@
+
+
+public class organizations {
+	
+	public static void main(String[] args) {
+		
+		
+		String name = "GFT";
+		String industry = "Banking";
+		String country = "Germany";
+		int size = 10000;
+	}
+}
