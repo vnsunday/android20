a927893e523f1517f985ae2c9d71c24e7d9d5038
vn_sunday <truyenspidermanvn@gmail.com>
2026-08-19 15:36:17 +0700

    anchor point. tension on string

---
+++ office-lt/apps/book-cover.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Condition to pass: having a point to the force lay-on
+//    
+bool pass_able(int V1, int V2, int anchorV, int* path, int npath) {
+    /*----------------------------------------
+        Assumption:
+            existing path is conditioned ()
+        Condition:
+            new point concatenating to existing path, must:
+                + Make sure the edge having tension-force on string
+     *----------------------------------------*/
+    // Make sure 
+    // Obstacle 
+}
+
+void path_finding() 
+{
+    /*============================================================
+        Input: 
+            Physical Node position
+        Expectation:
+            Finding a path
+            At-lest two string-depth at some edges
+        Output
+            A path
+     *============================================================*/
+    /* 
+        A1    B1
+        A2    B2
+     */
+    int pIA1, pIB1, pIA2, pIB2;
+    int pOA1, pOB1, pOA2, pOB2;
+    int nstring; // String of thread 
+
+    // Define obstacle 
+    int nAC; // Anchor Point
+    
+}
+
+int main(int argc, char const *argv[])
+{    
+    return 0;
+}
