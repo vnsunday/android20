859adf4aee6d01b331584917291fcc8a5da926f9
vnsunday <truyenspidermanvn@gmail.com>
2026-08-20 19:50:28 +0700

    relative position

--- office-lt/apps/book-cover.cpp
+++ office-lt/apps/book-cover.cpp
@@ -1,5 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string>
+
+using namespace std;
+
+/*============================================================
+    Horizontal Line: 
+        Y = a number
+    Vertical Line:
+        X = a number
+
+ *============================================================*/
+bool pass_able_2D(int X, int Y, bool bHorizon = true) {
+
+    return true;
+}
+
+/*============================================================
+    How the line [S,A] position compare to the line [A,B]?
+        (a) Clock-wise:
+            If (B,S) is clock wise compare to A
+        (b) Counter clock-wise
+            If (B,S) is counter-clock-wise compare to A
+ *============================================================*/
+std::string relative_position(int nAX, int nAY, int nBX, int nBY, 
+    int nSX,
+    int nSY)
+{ 
+    return "cw"; // Clockwise
+}
+
+/*============================================================
+    nAX: anchor Point X 
+    nAY: anchor Point Y 
+    nFX: final Point X 
+    nFY: final Point Y
+ *============================================================*/
+void pull_string(int nAX, int nAY, int nFX, int nFY) {
+    int** azPath = NULL;
+
+    int nP; // n path 
+    int nA2X;
+    int nA2Y;
+    
+    nAX = 0;
+    nAY = 0;
+
+    nA2Y = nAY;
+    nA2X = nAX + 10;
+}
+
+
+
+bool drill_a_hole(int X, int Y, bool bHorizon, int Xh, int Yh) {
+    
+    return true;
+}
 
 // Condition to pass: having a point to the force lay-on
 //    
@@ -13,6 +69,7 @@ bool pass_able(int V1, int V2, int anchorV, int* path, int npath) {
      *----------------------------------------*/
     // Make sure 
     // Obstacle 
+    return true;
 }
 
 void path_finding() 
@@ -36,10 +93,20 @@ void path_finding()
 
     // Define obstacle 
     int nAC; // Anchor Point
-    
 }
 
 int main(int argc, char const *argv[])
 {    
+    int define2D = 0;
+    bool bHorizontal = false;
+
+    // Define 2D line: Horizontal or Vertical
+    int X; // 
+    int Y; 
+    bool bVertical = true;
+
+    int path;
+    
+
     return 0;
 }
